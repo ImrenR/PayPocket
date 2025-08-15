657e9eaa99a776bec6d40b82f143926a8ee67713
ImrenR <imrenrahbay@gmail.com>
2025-08-15 21:56:44 +0100

    some fixes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//! ********** TO ADD INCOMES ON THE FORM ***********************//
+
 const incomeInput = document.querySelector("#incomeInput")
 const added = document.querySelector("#added")
 const addForm = document.querySelector("#addForm")
@@ -5,7 +7,7 @@ let incomes = 0;
 
 addForm.addEventListener("submit", (e)=>{
   e.preventDefault()
-  incomes = Number(incomeInput.value)
+  incomes = incomes + Number(incomeInput.value)
  console.log(incomes);
  localStorage.setItem('income', incomes)
 })
@@ -13,3 +15,5 @@ addForm.addEventListener("submit", (e)=>{
 // ! To able to see and store your incomes:
 // const seeIncomes =localStorage.getItem(incomes)
 // console.log(seeIncomes)
+
+//! ******************************************************** */
