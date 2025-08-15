34cfdcf3cbb850720fea07b311c7adfe30caa473
ImrenR <imrenrahbay@gmail.com>
2025-08-15 21:16:29 +0100

    enter your income

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const add = document.querySelector("add")
+const incomeInput = document.querySelector("incomeInput")
+const addForm = document.querySelector("addForm")
+let income = 0;
+
+addForm.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  income = incomeInput.value
+
+})
