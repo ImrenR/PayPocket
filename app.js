ca97f40db0e10834be4901ca05ef2f725a170cb2
ImrenR <imrenrahbay@gmail.com>
2025-08-15 22:18:44 +0100

    update

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const added = document.querySelector("#added");
 const addForm = document.querySelector("#addForm");
 const showIncome = document.querySelector("#showIncome");
 let incomes = JSON.parse(localStorage.getItem("incomes")) || 0;
+// JSON parse : we use this to not have string value
 
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   incomes = incomes + Number(incomeInput.value);
-  incomes = showIncome.textContent
-  console.log(incomes);
+  showIncome.textContent = incomes;
   localStorage.setItem("income", incomes);
 });
+
 
 // ! To able to see and store your incomes:
 // const seeIncomes =localStorage.getItem(incomes)
