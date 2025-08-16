0e86fb66b7c6da7e528768a8f1bd6ddf53acbe98
ImrenR <imrenrahbay@gmail.com>
2025-08-16 18:45:55 +0100

    final

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 //! ********** TO ADD INCOMES ON THE FORM ***********************//
 
-const incomeInput = document.querySelector("#incomeInput");
+const incomeInput = document.querySelector("#incomeInput"); 
 const added = document.querySelector("#added");
 const addForm = document.querySelector("#addForm");
 const showIncome = document.querySelector("#showIncome");
-let incomes = JSON.parse(localStorage.getItem("incomes")) || 0; // to avoid strings we used Parse: such as 20002000 will be with parse 2000
+let incomes = JSON.parse(localStorage.getItem("myIncomes")) || 0; // to avoid strings we used Parse: such as 20002000 will be with parse 2000
 // JSON parse : we use this to not have string value
 
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   incomes = incomes + Number(incomeInput.value);
-  showIncome.textContent = incomes;
-  localStorage.setItem("income", incomes);
+  
+  localStorage.setItem("myIncomes", incomes);
 });
 
 // ! To able to see and store your incomes:
@@ -39,16 +39,17 @@ expensesForm.addEventListener("submit", (e) => {
   listOfExpenses.push(newExpenses);
   localStorage.setItem("expense", JSON.stringify(listOfExpenses));
     expencesOfTable(newExpenses)
+ 
 });
 
 listOfExpenses.forEach((expenses) => {
   expencesOfTable(expenses);
 });
 
-//! ****************PRINTING THE VALUES ON THE TABLE**********************************************************/
+//! **************** PRINTING THE VALUES ON THE TABLE**********************************************************/
+
 
 // if you need to update the values all the time you need to use functions!
-
 function expencesOfTable({ id, quantity, date, information }) {
   expensesTable.innerHTML += `
   <tr>
@@ -68,3 +69,35 @@ function expencesOfTable({ id, quantity, date, information }) {
      }
   });
 }
+
+//! ******************* CALCULATION ***********************************************/
+
+const showExpenses = document.querySelector("#showExpenses")
+const showMoneyLeft = documentquerySelector("#showMoneyLeft")
+
+
+
+function hesaplaGoster () {
+  const harcamaToplami = listOfExpenses.reduce((acc,item)=> acc + item.quantity , 0)
+  const gelir = JSON.parse(localStorage.getItem("myIncomes"))
+  showIncome.textContent = gelir;
+  showExpenses.textContent = harcamaToplami
+  showMoneyLeft.textContent =  gelir - harcamaToplami
+}
+hesaplaGoster()
+
+
+//! ************************************* CLEAR ALL *************************************************/
+
+const clearIt = document.querySelector("#clearIt")
+
+clearIt.addEventListener("click", ()=>{
+  incomeInput.value= ""
+  listOfExpenses.value= []
+  myIncomes= 0
+  expencesOfTable.innerHTML =""
+  hesaplaGoster()
+  localStorage.removeItem("expence")
+  localStorage.removeItem("myIncomes")
+
+})
